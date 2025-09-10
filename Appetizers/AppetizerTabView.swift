061c964a363c68e684b158cfafb80134cd7a742d
murad <muradkv@gmail.com>
2025-09-10 21:34:46 +0300

    Initial commit

---
+++ Appetizers/AppetizerTabView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by murad on 10.09.2025.
+//
+
+import SwiftUI
+
+struct AppetizerTabView: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    AppetizerTabView()
+}
---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by murad on 10.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerTabView()
+        }
+    }
+}
