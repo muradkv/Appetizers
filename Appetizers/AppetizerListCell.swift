03e1f20a587ae5632dc7b5298cd246ab38128179
murad <muradkv@gmail.com>
2025-09-13 06:38:30 +0300

    Add AppetizerListCell for custom list item UI
    - Created `AppetizerListCell` view with image, name, and price for displaying Appetizer items.
    - Styled cell with title2 font for name, semibold for price, and secondary color for price text.
    - Added fixed-size image with rounded corners and layout using HStack and VStack.
    - Integrated `AppetizerListCell` into List in AppetizerListView for rendering appetizers.
    - Included #Preview with sampleAppetizer for UI testing in Xcode Canvas.
    - Added sampleImage asset for test displaying

---
+++ Appetizers/AppetizerListCell.swift
@@ -0,0 +1,36 @@
+//
+//  AppetizerListCell.swift
+//  Appetizers
+//
+//  Created by murad on 13.09.2025.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    let appetizer: Appetizer
+    
+    var body: some View {
+        HStack {
+            Image(.sample)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 120, height: 90)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                Text("$\(appetizer.price, specifier: "%.2f")")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.secondary)
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+#Preview {
+    AppetizerListCell(appetizer: MockData.sampleAppetizer)
+}
